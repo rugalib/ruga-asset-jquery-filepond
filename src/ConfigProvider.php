da167038a1b210a1bc4430a9c09e142c0a30f0ae
Roland Rusch <roland@rusch.lu>
2023-12-04 10:53:35 +0100

    Initial commit

---
+++ src/ConfigProvider.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * SPDX-FileCopyrightText: 2023 Roland Rusch, easy-smart solution GmbH <roland.rusch@easy-smart.ch>
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+declare(strict_types=1);
+
+namespace Ruga\Asset\Skeleton;
+
+/**
+ * ConfigProvider.
+ *
+ * @author Roland Rusch, easy-smart solution GmbH <roland.rusch@easy-smart.ch>
+ */
+class ConfigProvider
+{
+    public function __invoke()
+    {
+        return [
+            'ruga' => [
+                'asset' => [
+                    'rugalib/ruga-asset-skeleton' => [
+                        'scripts' => [],
+                        'stylesheets' => [],
+                    ],
+                ],
+            ],
+        ];
+    }
+}
